1b0bd032b6f84a3298fcb05b7f1faa02981a10fe
Rob Terhaar <rob@atlanticdynamic.com>
2012-04-04 17:17:55 -0400

    wip

--- plot.py
+++ plot.py
@@ -3,11 +3,27 @@ import matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.mlab as mlab
+import numpy as np
+
+import csv
 
 # help from: 
 # http://www.prettygraph.com/blog/how-to-plot-a-scatter-plot-using-matplotlib/
 
-def chart_it(csv_r, chart_name='mew', color='red'):
+def index_finder(l):
+    prev = 0
+    indexes = []
+    for r in l:
+        if r['data'] == prev:
+            print r
+        else:
+            indexes.append(r['data'])
+            print r
+        prev = r['data']
+    return indexes
+
+
+def chart_it(csv_r, chart_name='mew', color='red', point_size=0.1):
     fig = Figure(figsize=(15,8))
 
     # Create a canvas and add the figure to it.
@@ -30,7 +46,9 @@ def chart_it(csv_r, chart_name='mew', color='red'):
 
     # Generate the Scatter Plot.
     for i in csv_r:
-        ax.scatter(i[0].skip, i[0].time, s=0.1, color=i[2], label=i[1])
+        a = np.array(i)
+        ax.scatter(a[0][1], a[0][2], s=point_size, color=i[2], label=i[1])
+        assert(False)
 
     # Save the generated Scatter Plot to a PNG file.
 #    canvas.print_figure(file_name.split('.')[0] + ".png", dpi=700)
@@ -40,16 +58,36 @@ def chart_it(csv_r, chart_name='mew', color='red'):
     canvas.print_figure(chart_name + ".png", dpi=700)
 
 
+def csv_data_summon(fp):
+    return [r for r in csv.DictReader(fp)]
 
-    
+def chart_prep(indexes, csv_data_list, file_name):
+    colors = 100*np.random.random(100).tolist()
+    l = []
+    for my_index in indexes:
+        my_index_list = []
+        for csv_data_chunk in csv_data_list:
+            if csv_data_chunk['data'] == my_index:
+                my_index_list.append(csv_data_chunk)
+        #l.append(my_index_list, file_name, colors.pop()))
+        l.append((my_index_list, "{0}_{1}".format(file_name, my_index), colors.pop()))
+    return l
+
+
+
 def main(args):
     # remove the first arg, the name of this script
     args.remove(args[0])
 
-    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-    l = []
+#    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+
     for file_name in args:
-        l.append((mlab.csv2rec(file_name), file_name, colors.pop()))
+        fp = open(file_name, 'rb')
+        csv_data_list = csv_data_summon(fp)
+        indexes = index_finder(csv_data_list)
+        l = chart_prep(indexes, csv_data_list, file_name)
+
+#        l.append((mlab.csv2rec(file_name), file_name, colors.pop()))
 
     chart_it(l)
 
